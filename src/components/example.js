fa4871fa344938a6b4604db6b560957ac3c1874c
EngerThen <engerthen4d17@gmail.com>
2019-10-14 19:48:19 -0400

    Added storybook.js to project

--- src/components/example.js
+++ src/components/example.js
@@ -4,5 +4,9 @@ import React from 'react';
  * Description of example component
  */
 
-const message = () => <h1>message</h1>;
-export default message;
+const Message = () => <h1>message</h1>;
+
+// I disable it for this line becasue I know you changed imports for the entire project. Travis warnign
+
+// eslint-disable-next-line import/prefer-default-export
+export { Message };
---
+++ src/components/example.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Message } from './example';
+
+// export the title of the Component as default story
+export default { title: 'Message' };
+
+// story for the component
+export const withText = () => <Message />;
